fc07ae87b48de2b5d2a867c36cb255aa4e4f1f49
Atayero Clinton <atayeroclinton.ac@gmail.com>
2024-05-10 15:00:32 +0530

    Second commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import "./navbar.scss";
+import { useState } from 'react';
 
 function Navbar() {
+    const [clicked, isClicked] = useState(false); 
+    // const user = true;
+
     return (
         <div className="navbar">
             <div className="left"><h2><a href=""> <span>Clin</span>Tech</a></h2></div>
             <div className="right">
-                <a href="">Home</a>
-                <a href="">About</a>
-                <a href="">Projects</a>
-                <a href="">Contact</a>
+                <a href="#homePage">Home</a>
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
             </div>
             <div className="menu">
-                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop"><img src="./menu.png" alt="" /></button>
+                <a class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop"><img src="./menu.png" alt="" /></a>
                 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasWithBackdrop" aria-labelledby="offcanvasWithBackdropLabel">
                     <div class="offcanvas-header">
                       <h2 class="offcanvas-title" id="offcanvasWithBackdropLabel"><span>Clin</span>Tech</h2>
-                      <button type="button" class="btn-close text-reset"  data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                      <button type="button" class="btn-close text-reset"  data-bs-dismiss="offcanvas" aria-label="Close" ></button>
                     </div>
                     <div class="offcanvas-body menu-links">
-                        <a href="">Home</a>
-                        <a href="">About</a>
-                        <a href="">Projects</a>
-                        <a href="">Contact</a>
+                        <a href="#homePage" data-bs-dismiss="offcanvas" onClick={(e) => {e.stopPropagation()}}>Home</a>
+                        <a href="#about" data-bs-dismiss="offcanvas" onClick={(e) => {e.stopPropagation()}}>About</a>
+                        <a href="#projects" data-bs-dismiss="offcanvas" onClick={(e) => {e.stopPropagation()}}>Projects</a>
+                        <a href="#contact" data-bs-dismiss="offcanvas" onClick={(e) => {e.stopPropagation()}}>Contact</a>
                     </div>
                 </div>
 
